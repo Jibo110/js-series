3215314482c909b5bf50d6dbfc6cc09557f0baec
Jibo110 <89148941+Jibo110@users.noreply.github.com>
2024-06-13 13:16:57 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountName = "Jibran"
+var accountPassword = "123456"
+accountState = "Islamabad"
+
+
+accountName = "Khan"
+accountState = "Rawalpindi"
+//console.log(accountId);
+console.table([accountId, accountName, accountPassword, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+// if you want to use new standards oj JavaScript then use "Use Strict", but now a days everything is in Use state, but it is good practice.
+
+"Use Strict";
