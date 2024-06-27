d12a96eae3f21b94c3565080adca27da121fa818
Jibo110 <89148941+Jibo110@users.noreply.github.com>
2024-06-27 11:46:50 +0000

    Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,27 @@
 // if you want to use new standards oj JavaScript then use "Use Strict", but now a days everything is in Use state, but it is good practice.
 
 "Use Strict";
+
+// alert (3+3), this won't run as we are using node not browsing
+
+console.log(3
+    + 
+    3
+) // code readibility
+
+let name = "Jibran"
+let age = 24
+let isLoggedIn = true
+
+//numbers => 2 power of 52
+// bigint
+// string => ""
+// boolean => true, false
+// null => standalone type
+// undefined 
+// symbol => unique
+
+// Go to https://tc39.es/ and read about datatypes
+
+
+// Object
