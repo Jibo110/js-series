8586fb134fcc83a6998f0d1b9ea7ae4c64fe1ca8
Jibo110 <89148941+Jibo110@users.noreply.github.com>
2024-06-27 12:20:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+let score = true
+
+console.log(score);
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN (type is number, confusing right?)
+// true = 1, false = 2.
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
